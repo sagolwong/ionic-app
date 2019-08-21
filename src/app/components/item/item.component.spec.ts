a28abca16af39c5e472b9098908af1919ab1a2c0
sagolwong <5910110678@psu.ac.th>
2019-08-21 19:22:07 +0700

    item component

---
+++ src/app/components/item/item.component.spec.ts
@@ -0,0 +1,39 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ItemComponent } from './item.component';
+import { TimeAgoPipe } from '../time-ago.pipe';
+import { TestUtils } from '../../../testing/test-utils';
+import { By } from '@angular/platform-browser';
+
+describe('ItemComponent', () => {
+  let component: ItemComponent;
+  let fixture: ComponentFixture<ItemComponent>;
+
+  beforeEach(async(() => {
+    TestUtils.beforeEachCompiler([
+      ItemComponent, TimeAgoPipe])
+      .then(compiled => {
+        fixture = compiled.fixture;
+        component = compiled.instance;
+      });
+  }));
+
+
+
+  it('should create', () => {
+    component.item = {
+      id: 1,
+      title: 'Test item 1',
+      url: 'http://www.example.com/test1',
+      by: 'user1',
+      time: 1478576387,
+      score: 242,
+    }
+    fixture.detectChanges()
+    expect(component).toBeTruthy();
+
+    const debugElement = fixture.debugElement.query(By.css('.title'))
+    expect(debugElement.nativeElement.textContent).toContain('Test item 1')
+  });
+});
